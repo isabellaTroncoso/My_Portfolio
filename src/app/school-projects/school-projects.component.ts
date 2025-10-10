5ab1039a7145bb6a3434506057c7f5ac9b798b22
isabellaTroncoso <isabella.troncoso@stud.sti.se>
2025-10-10 21:23:41 +0200

    feat: add video intro, profile, school projects, and improved UI

--- src/app/school-projects/school-projects.component.ts
+++ src/app/school-projects/school-projects.component.ts
@@ -1,8 +1,10 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
+import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-school-projects',
-  imports: [],
+  imports: [CommonModule, RouterLink],
   templateUrl: './school-projects.component.html',
   styleUrl: './school-projects.component.css'
 })
